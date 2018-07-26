2e9e2a025879c45fdc293bc30c0f2c170489f99c
mason <masonaesposito@gmail.com>
2018-07-26 14:39:35 -0700

    final deployment

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -38,3 +38,12 @@ describe('server/app.js', function() {
     });
   });
 })
+it('page says You\'re serving up the public folder, but do your tests pass?', (done) => {
+  chai.request(server)
+    .get('/')
+    .end((err, res) => {
+      expect(err).not.exist;
+      expect(JSON.stringify(res.text)).to.contain('You\'re serving up the public folder, but do your tests pass?');
+      done();
+    });
+  });
